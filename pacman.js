cafe804f14311d6c750b0c2846b6309599a10a60
Casey Arnold <casey.arnold@me.com>
2020-12-28 16:14:57 -0500

    first commit

---
+++ pacman.js
@@ -0,0 +1,149 @@
+window.addEventListener('load', () => {
+  var gameboard = document.querySelector('.gameboard')
+  var height = 30
+  var width = 27
+  var currentPacmanPos = 390
+  var gameScore = 0
+  var pointsPerDot = 10
+  var currentDirection = ''
+
+  var GAME_SETUP = [
+    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    renderBoard()
+
+    window.addEventListener('keyup', (event) => {
+      if(event.key === 'ArrowUp') currentDirection = 'goUp'
+      if(event.key === 'ArrowDown') currentDirection = 'goDown'
+      if(event.key === 'ArrowLeft') currentDirection = 'goLeft'
+      if(event.key === 'ArrowRight') currentDirection = 'mvRight'
+    })
+
+    function isMovable(pos, curr) {
+      // cannot move into obstacle
+      if(GAME_SETUP[pos] === 1) {
+        return false
+      }
+      // cannot move into an undefined div elem
+      if(pos <= 0 || pos >= 811) {
+        return false
+      }
+
+      // cannot move towards edge
+      if(isEdge(pos, curr)) {
+        return false
+      }
+
+      return true
+    }
+
+    function isEdge(curr, pos) {
+      if(curr % 27 === 26 && pos % 27 === 0) {
+        return true
+      }
+
+      if(curr % 27 === 0 && pos % 27 === 26) {
+        return true
+      }
+
+      return false
+    }
+
+    function goUp () {
+      if(isMovable(currentPacmanPos - 27, currentPacmanPos)) {
+        movePacman(currentPacmanPos - 27)
+      }
+    }
+
+    function goDown () {
+      if(isMovable(currentPacmanPos + 27, currentPacmanPos)) {
+        movePacman(currentPacmanPos + 27)
+      }
+    }
+
+    function mvRight () {
+      if(isMovable(currentPacmanPos + 1, currentPacmanPos)) {
+        movePacman(currentPacmanPos + 1)
+      }
+    }
+
+    function goLeft () {
+      if(isMovable(currentPacmanPos - 1, currentPacmanPos)) {
+        movePacman(currentPacmanPos - 1)
+      }
+    }
+
+    function movePacman(pos) {
+      if(GAME_SETUP[pos] == 0) { // addScore
+        gameScore += pointsPerDot
+        updateScore()
+      }
+      GAME_SETUP[pos] = 2
+      GAME_SETUP[currentPacmanPos] = 9
+      currentPacmanPos = pos
+      clearBoard()
+      renderBoard()
+    }
+
+    function updateScore () {
+      var scorebox = document.querySelector('.score p')
+      scorebox.innerHTML = gameScore
+    }
+
+    function clearBoard () {
+      gameboard.innerHTML = ""
+    }
+
+    function renderBoard () {
+      for(i = 0; i < (height * width); i++) {
+        var div = document.createElement("div")
+
+        if(GAME_SETUP[i] == 0) {
+          div.innerHTML = "&bullet;"
+          // div.innerHTML = i
+          div.setAttribute("class", "point-box")
+        } else if(GAME_SETUP[i] == 1) {
+          div.setAttribute("class", "obstacle")
+        } else if(GAME_SETUP[i] == 2) { // pacman
+          div.setAttribute("class", "pacman")
+        } else if(GAME_SETUP[i] == 9) { // empty tile
+          div.setAttribute("class", "empty")
+        }
+
+        gameboard.appendChild(div)
+      }
+    }
+
+    setInterval(() => {
+      if(currentDirection != '') eval(currentDirection + "()")
+    }, 220)
+})
